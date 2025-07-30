52eb4f5b3c807b91b2530f07e8e4de7b7ca3f8e6
ram-chadar <javaprojectwork2715@gmail.com>
2025-07-30 12:08:08 +0530

    initial

---
+++ employee/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
